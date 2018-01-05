9278bf3a3fd8b03accadd81d0b1bfcf78cac335a
황진수-PC <cjswp122@gmail.com>
2018-01-05 17:09:08 +0900

    sale list insert or update

--- routes/saleList.js
+++ routes/saleList.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import isEmpty from 'lodash/isEmpty';
 // db config
 import connection from '../helper/dbConnect';
 import api from '../helper/api';
@@ -48,39 +49,79 @@ router.get('/', async (req, res) => {
       end_date,
     } = goods;
 
+    // connection.query(`SELECT goodsno FROM gd_goods WHERE goodscd='1000123'
+    connection.query(`SELECT goodsno FROM gd_goods WHERE goodscd='${goods_id}'
+    `, (err, selectResult) => {
+        if (err) console.log(err);
+        if (isEmpty(selectResult)) {
+          // open <- 상품 진열 하기
+          connection.query(`INSERT INTO gd_goods SET 
+          goodsnm='${goods_nm}', 
+          img_i='${goods_img}', 
+          img_s='${goods_img}', 
+          img_m='${goods_img}', 
+          img_l='${goods_img}', 
+          goodscd='${goods_id}',
+          goods_price='${real_price}', 
+          maker='${goods_com_name}',
+          open=1
+          `, (error, insertResult) => {
+            if (error) console.log('insert gd_goods err: ', error);
+            connection.query(`INSERT INTO gd_goods_link SET 
+            goodsno='${insertResult.insertId}',
+            category='001',
+            sort=-unix_timestamp()-${insertResult.insertId}
+            `, (error, subInsertResult) => {
+              if (error) console.log('insert gd_goods_link err: ', error);
+              console.log(subInsertResult);
+            });
 
-    // open <- 상품 진열 하기
-    connection.query(`INSERT INTO gd_goods SET 
-    goodsnm='${goods_nm}', 
-    img_i='${goods_img}', 
-    img_s='${goods_img}', 
-    img_m='${goods_img}', 
-    img_l='${goods_img}', 
-    goodscd='${goods_id}',
-    goods_price='${real_price}', 
-    maker='${goods_com_name}',
-    open=1
-    `, (error, result, fields) => {
-      if (error) console.log(error);
-      connection.query(`INSERT INTO gd_goods_link SET 
-      goodsno='${result.insertId}',
-      category='001',
-      sort=-unix_timestamp()-${result.insertId}
-      `, (error, result, fields) => {
-        if (error) console.log(error);
-        console.log(result);
-      });
+            connection.query(`INSERT INTO gd_goods_option SET 
+            goodsno='${insertResult.insertId}',
+            price='${real_price}',
+            link='1',
+            optno='1'
+            `, (error, subInsertResult) => {
+              if (error) console.log('insert gd_goods_option err: ', error);
+              console.log(subInsertResult);
+            });
 
-      connection.query(`INSERT INTO gd_goods_option SET 
-      goodsno='${result.insertId}',
-      price='${real_price}',
-      link='1',
-      optno='1'
-      `, (error, result, fields) => {
-        if (error) console.log(error);
-        console.log(result);
+            return res.status(200);
+          });
+        } else {
+          connection.query(`UPDATE gd_goods SET 
+          goodsnm='omg', 
+          img_i='${goods_img}', 
+          img_s='${goods_img}', 
+          img_m='${goods_img}', 
+          img_l='${goods_img}', 
+          goods_price='${real_price}', 
+          maker='${goods_com_name}'
+          WHERE goodscd='${goods_id}'
+          `, (error, updateResult) => {
+            if (error) console.log('update gd_goods err: ', error);
+            console.log('update, ', updateResult);
+            const { goodsno } = selectResult[0];
+            connection.query(`UPDATE gd_goods_link SET 
+            category='002'
+            WHERE goodsno='${goodsno}'
+            `, (error, subUpdateResult) => {
+              if (error) console.log('update gd_goods_link err: ', error);
+              console.log(subUpdateResult);
+            });
+
+            connection.query(`UPDATE gd_goods_option SET 
+            price='${real_price}'
+            WHERE goodsno='${goodsno}'
+            `, (error, subUpdateResult) => {
+              if (error) console.log('update gd_goods_link err: ', error);
+              console.log(subUpdateResult);
+            });
+
+            return res.status(200);
+          });
+        }
       });
-    });
   });
 
   console.log('DONE');
