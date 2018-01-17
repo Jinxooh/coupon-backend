a78d38a04e13aef5fb7973446ddc65c90ca45bd2
황진수-PC <cjswp122@gmail.com>
2018-01-17 15:50:25 +0900

    scheduler 7am setting

--- helper/scheduler.js
+++ helper/scheduler.js
@@ -2,13 +2,23 @@ import schedule from 'node-schedule';
 import fetch from 'node-fetch';
 import logger from './logger';
 
-const BACKEND_SERVER = process.env.NODE_ENV === 'development' ? process.env.TEST_BACKEND_SERVER : process.env.BACKEND_SERVER;
+const BACKEND_SERVER = process.env.SERVER === 'test' ? process.env.TEST_BACKEND_SERVER : process.env.BACKEND_SERVER;
 
 const start = () => {
   logger.info('scheduler init');
   schedule.scheduleJob('0 0 7 * * 0-5', () => { // am 7 , mon-fri
     logger.info('scheduler on');
-    fetch(`${BACKEND_SERVER}saleList`, { method: 'GET' }).catch((error) => { logger.error(error); });
+    fetch(`${BACKEND_SERVER}saleList`, { method: 'GET' })
+    .then((rsp) => rsp.text())
+    .then((result) => {
+      const { success } = JSON.parse(result);
+      if (success) {
+        logger.info('salelist insert ok');
+      } else {
+        logger.error('ERROR: salelist');
+      }
+    })
+    .catch((error) => { logger.error(error); });
   });
 }
 
