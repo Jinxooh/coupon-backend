0ab42cf454abdfbbee2f417a8a9ff475d6430bc2
황진수-PC <cjswp122@gmail.com>
2018-02-08 14:05:29 +0900

    coupon 상태 체크

--- src/routes/couponStatus.js
+++ src/routes/couponStatus.js
@@ -6,19 +6,19 @@ import dataType from '../helper/dataType';
 const router = express.Router();
 
 router.post('/', async (req, res, next) => {
-  console.log('couponStatus, ', req.body);
+  // console.log('couponStatus, ', req.body);
   const {
     tr_order_id,
   } = req.body;
 
   const response = await api.callAPI('coupon_status.asp', dataType.couponStatus(tr_order_id))
     .catch(next);
-  const { StatusCode } = response.result;
-  console.log('StatusCode', StatusCode);
-  if (StatusCode === '0') {
-    return res.status(200).json({ success: response });
+  const { StatusCode: code } = response.result;
+  // console.log('StatusCode', code);
+  if (code === '0') {
+    return res.status(200).json({ code });
   }
-  return res.status(400).json({ failure: response });
+  return next(code);
 });
 
 export default router;
